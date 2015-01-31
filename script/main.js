b2231caaa080d4f049c7a1edcb30047015765fda
vikmaksimenko <vikmakisimenko@hotmail.com>
2015-01-31 16:43:14 +0200

    Devided code into methods and created soting method

--- script/main.js
+++ script/main.js
@@ -42,48 +42,78 @@
     //   //network.redraw();
     // });
 
-   $(document).ajaxError(function(a, b, c, d) {
-       console.log("error" + d);
-   });
+    $(document).ajaxError(function(a, b, c, d) {
+        console.log("error" + d);
+    });
 
 
 
-   $(document).ready(function() {
-       // $.getJSON('/data/data_for_sigma.json', function(json) {
-       //     console.log(json);
-       //     init(json);
+    $(document).ready(function() {
+        // $.getJSON('/data/data_for_sigma.json', function(json) {
+        //     console.log(json);
+        //     init(json);
 
 
-       // });
-      init(data);
-   });
+        // });
+        init(data);
+    });
 
     function init(json) {
-var nodes = json.nodes;
+        var nodes = json.nodes;
+        preformatData(json);
+        //        sortNodesToCircle(nodes);
+        sortNodesToLines(nodes);
+        initGraph(json);
+    }
 
-           for (var i = 0; i < nodes.length; i++) {
-               nodes[i].x = 100 * Math.cos(2 * i * Math.PI / nodes.length);
-               nodes[i].y = 100 * Math.sin(2 * i * Math.PI / nodes.length);
-               nodes[i].size = 2;
-           }
-
-           var edges = json.edges;
-           for (var i = 0; i < edges.length; i++) {
-               edges[i].id = i + "";
-               //edges[i].type = "arrow";
-
-           }
-
-           sigma.renderers.def = sigma.renderers.canvas;
-             s = new sigma({
-               container: document.getElementById('container'),
-               graph: json,
-               settings: {
+    function initGraph(json) {
+        sigma.renderers.def = sigma.renderers.canvas;
+        s = new sigma({
+            container: document.getElementById('container'),
+            graph: json,
+            settings: {
                 labelThreshold: 10
-               }
-           });
+            }
+        });
 
-            // Initialize the dragNodes plugin:
-            var dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
-           //s.startForceAtlas2({worker: true, barnesHutOptimize: false});
+        // Initialize the dragNodes plugin:
+        var dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
+        //s.startForceAtlas2({worker: true, barnesHutOptimize: false});
+    }
+
+    function preformatData(json) {
+        var edges = json.edges;
+        for (var i = 0; i < edges.length; i++) {
+            edges[i].id = i + "";
+            edges[i].type = "arrow";
+        }
+
+    }
+
+    function sortNodesToCircle(nodes) {
+        for (var i = 0; i < nodes.length; i++) {
+            nodes[i].x = 100 * Math.cos(2 * i * Math.PI / nodes.length);
+            nodes[i].y = 100 * Math.sin(2 * i * Math.PI / nodes.length);
+            nodes[i].size = 2;
+        }
+
+    }
+
+    var mem;
+
+    function sortNodesToLines(nodes) {
+        mem = nodes.reduce(function(memo, item) {
+            if (!memo[item.level]) memo[item.level] = [];
+            memo[item.level].push(item);
+            return memo;
+        }, {});
+        console.log(mem);
+
+        for (var i = 0; i < mem.length; i++) {
+            for (var j = 0; j < level.length; j++) {
+                mem[i][j].x = (mem[i].length + 1) / j;
+                mem[i][j].y = (mem.length + 1) / i;
+            }
+        }
+        console.log(mem);
     }
